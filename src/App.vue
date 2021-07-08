fb77a8c34b6add141c3462e998fa795cf9f76176
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-07-08 19:07:54 +0300

    working with form

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,30 @@ import TheForm from './components/TheForm.vue';
 
 export default {
   components: {
-    TheForm
-  }  
-}
+    TheForm,
+  },
+};
 </script>
 
 <style>
-* {
+*,
+::after,
+::before {
   box-sizing: border-box;
 }
 
 html {
-  font-family: sans-serif;
+  font-family: 'Noto Sans', serif;
+}
+
+img {
+  display: block;
 }
 
 body {
   margin: 0;
-  background-color: #292929;
+  background-color: #eee;
+  line-height: 1.5;
+  color: #373a3c !important;
 }
 </style>
